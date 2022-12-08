dc2b12c68b1e42b014847cc45cb1d3e7ada346c1
egenbia <biahegen1@gmail.com>
2022-12-08 18:02:08 -0300

    fim

---
+++ js/funcoes.js
@@ -0,0 +1,71 @@
+//Criar uma variável para manipular o XML
+xmlhttp = new XMLHttpRequest();
+//Seleciona o arquivo XML
+xmlhttp.open("GET","xml/conteudo.xml",false);
+//Enviar o arquivo para a leitura
+xmlhttp.send();
+//Informar que o arquivo enviado é um XML
+xmlDoc = xmlhttp.responseXML;
+
+x = xmlDoc.getElementsByTagName("conteudo")
+y = xmlDoc.getElementsByTagName("noticia")
+
+function textoODS() {
+	for (i=x.length-1;i>=0;i--) {
+		document.write("<p class='text-left' id='text-ods'><strong>" + x[i].getElementsByTagName("inicioods")[0].childNodes[0].nodeValue + "</strong>" + x[i].getElementsByTagName("parteumods")[0].childNodes[0].nodeValue + "<br>" + x[i].getElementsByTagName("partedoisods")[0].childNodes[0].nodeValue + "<br>" + x[i].getElementsByTagName("partetresods")[0].childNodes[0].nodeValue + "<br>" + x[i].getElementsByTagName("partequatroods")[0].childNodes[0].nodeValue +"</p>");}
+}
+
+function postagemPequena() {
+for(i= y.length-1;i>=0;i--){
+	resto = i % 2;
+	if(resto == 1){
+	document.write("<div class='row'><div class='col-md-1'></div><div class='col-md-5'> <div class='row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative'> <div class='col p-4 d-flex flex-column position-static'> <strong class='d-inline-block mb-2 text-primary'>ODS 14</strong><h4 class='mb-0'>" + y[i].getElementsByTagName("titulo")[0].childNodes[0].nodeValue + "</h4> <div class='mb-1 text-muted'>"+  y[i].getElementsByTagName("data")[0].childNodes[0].nodeValue +"</div><p class='card-text mb-auto'>"+  y[i].getElementsByTagName("corpo")[0].childNodes[0].nodeValue.substr(0,100) + "...</p><a href='noticia.html?codigo_noticia=" + i + "' class='stretched-link'>Continue Lendo...</a></div><div class='col-auto d-none d-lg-block'><img src='imgs/"+ y[i].getElementsByTagName("imagem")[0].childNodes[0].nodeValue +"' width='200' height='250'></div></div></div>");
+	}
+	else{
+		document.write("<div class='col-md-5'> <div class='row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative'> <div class='col p-4 d-flex flex-column position-static'> <strong class='d-inline-block mb-2 text-primary'>ODS 14</strong><h4 class='mb-0'>" + y[i].getElementsByTagName("titulo")[0].childNodes[0].nodeValue + "</h4> <div class='mb-1 text-muted'>"+  y[i].getElementsByTagName("data")[0].childNodes[0].nodeValue +"</div><p class='card-text mb-auto'>"+  y[i].getElementsByTagName("corpo")[0].childNodes[0].nodeValue.substr(0,100) + "...</p><a href='noticia.html?codigo_noticia=" + i + "' class='stretched-link'>Continue Lendo...</a></div><div class='col-auto d-none d-lg-block'><img src='imgs/"+ y[i].getElementsByTagName("imagem")[0].childNodes[0].nodeValue +"' width='200' height='250'></div></div></div></div>");
+	}
+}
+}
+
+function corpoPostagem(){
+    url = new URL(window.location.href)
+    parametro = url.searchParams;
+    i = parametro.get("texto"); 
+
+   var corpo = y[i].getElementsByTagName("texo")[0].childNodes[0].nodeValue;
+   var corpopronto = corpo.replace("\n","<br>")
+	document.write(corpopronto)
+}
+
+
+function tituloPostagem(){
+    url = new URL(window.location.href)
+    parametro = url.searchParams;
+    i = parametro.get("texto"); 
+
+	document.write(y[i].getElementsByTagName("textoODS")[0].childNodes[0].nodeValue)
+}
+
+function dataPostagem(){
+    url = new URL(window.location.href)
+    parametro = url.searchParams;
+    i = parametro.get("metas"); 
+
+	document.write(y[i].getElementsByTagName("data")[0].childNodes[0].nodeValue)
+}
+
+function imagemPostagem(){
+    url = new URL(window.location.href)
+    parametro = url.searchParams;
+    i = parametro.get("metas"); 
+    
+    document.write("<center><img src='imgs/"+ y[i].getElementsByTagName("logo")[0].childNodes[0].nodeValue +"' width='80%' style='margin-top: 00px;border-radius: 10px;'></center>")
+}
+
+function fontePostagem(){
+    url = new URL(window.location.href)
+    parametro = url.searchParams;
+    i = parametro.get("metas"); 
+
+	document.write("fonte: "+y[i].getElementsByTagName("3.1")[0].childNodes[0].nodeValue)
+}
